60e48dacc8af73eb6637530e3d7395f6f28f6f62
Uju Uzoagu <ouzoagu@students.kennesaw.edu>
2022-07-07 17:13:59 +0000

    datatupdate

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS weather;
 USE weather;
 
 CREATE TABLE catalog(
-    address VARCHAR(255) default NULL,
+    city VARCHAR(255) default NULL,
     temp VARCHAR(255) default NULL,
     date DATE default NULL
 ) AUTO_INCREMENT=1;
