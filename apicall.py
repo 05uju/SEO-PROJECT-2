6c70749808bce5a222fb5dc107d8cdc2f12a74ff
Mary Awopileda <maryawopileda@gmail.com>
2022-07-07 20:35:10 +0000

    updates

--- apicall.py
+++ apicall.py
@@ -1,21 +1,20 @@
 import requests
-from datetime import date
+import datetime
+
+
 
 url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 
 city = input('Enter your city: ')
-days = input('How many days in advance do you want the weather for?: ')
-today = date.today()
+offset = input('How many days in advance do you want the weather for?: ')
+date = datetime.date.today() + datetime.timedelta(days=int(offset))
 
-querystring = {"q":city,"days":days,"dt":today}
+querystring = {"q":city,"days":offset,"dt":date}
 
 headers = {
 	"X-RapidAPI-Key": "09d4e9e5cfmsh53cd4e0fd2d327ep1ec662jsnda2c540f8b14",
 	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
 }
-#print(response.text)
-
-#print(response['current']['temp_f'])
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 response = response.json()
--- database.sql
+++ database.sql
@@ -1,12 +1,9 @@
-CREATE DATABASE IF NOT EXISTS weather_data;
-USE weather_data;
+CREATE DATABASE IF NOT EXISTS weather;
+USE weather;
 
 CREATE TABLE catalog(
     city VARCHAR(255) default NULL,
     temp VARCHAR(255) default NULL,
-    date DATE() default NULL
+    date DATE default NULL
 ) AUTO_INCREMENT=1;
 
-INSERT INTO catalog(city, temp, date)
-VALUES ("Kennesaw", 65, 07-06-2022);
-SELECT * FROM catalog;
