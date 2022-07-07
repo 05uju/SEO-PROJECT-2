4576ff30f18854b2208cdeb715a80572dc1f7766
Uju Uzoagu <ouzoagu@students.kennesaw.edu>
2022-07-07 20:48:26 +0000

    updated

--- database.sql
+++ database.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE IF NOT EXISTS weather_data;
 USE weather_data;
 
-CREATE TABLE catalogg (
+CREATE TABLE IF NOT EXISTS weather (
     City VARCHAR(255) default NULL,
     Temperature VARCHAR(255) default NULL,
     Dates DATE default NULL
 ) AUTO_INCREMENT=1;
-
-INSERT INTO catalogg(City, Temperature, Dates)
