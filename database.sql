96767dccc2c15dc8da47db10f9c5bca2e4c57c9c
Uju Uzoagu <ouzoagu@students.kennesaw.edu>
2022-07-07 17:03:48 +0000

    message

--- database.sql
+++ database.sql
@@ -4,9 +4,6 @@ USE weather;
 CREATE TABLE catalog(
     address VARCHAR(255) default NULL,
     temp VARCHAR(255) default NULL,
-    date DATE() default NULL
+    date DATE default NULL
 ) AUTO_INCREMENT=1;
 
-INSERT INTO catalog(address, temp, date)
-VALUES ("Kennesaw", 65, 07-06-2022);
-SELECT * FROM catalog;
