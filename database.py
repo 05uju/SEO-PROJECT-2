4f3f76cc0fc8814f2c636747d39072ab284e242a
Uju Uzoagu <ouzoagu@students.kennesaw.edu>
2022-07-07 04:13:26 +0000

    message

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 CREATE DATABASE IF NOT EXISTS weather_data;
+USE weather_data;
 
 CREATE TABLE catalog(
     address VARCHAR(255) default NULL,
     temp VARCHAR(255) default NULL,
     date DATE() default NULL
 ) AUTO_INCREMENT=1;
+
+INSERT INTO catalog(address, temp, date)
+VALUES ("Kennesaw", 65, 07-06-2022);
