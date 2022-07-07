870fb78699ad37b5da7aca7ce1b904ead1c46731
Uju Uzoagu <ouzoagu@students.kennesaw.edu>
2022-07-07 17:59:04 +0000

    update

--- database.sql
+++ database.sql
@@ -1,4 +1,10 @@
 CREATE DATABASE IF NOT EXISTS weather;
 USE weather;
 
-INSERT INTO catalog(City, Temperature, Dates)
+CREATE TABLE column (
+    City VARCHAR(255) default NULL,
+    Temperature VARCHAR(255) default NULL,
+    Dates DATE default NULL
+) AUTO_INCREMENT=1;
+
+INSERT INTO column(City, Temperature, Dates)
